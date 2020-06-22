46f666111d22de100816d4f53adbdefea141ebad
dominq10 <33425384+dominq10@users.noreply.github.com>
2020-06-22 19:19:23 +0700

    Update dark_theme.py

--- style/dark_theme.py
+++ style/dark_theme.py
@@ -1,3 +1,17 @@
+#Copyright 2020 Pavel Tolkachev
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <https://www.gnu.org/licenses/>.
 window_css = "background-color: #1a222c"
 label_css = "font: 12px; color: #c2cdd9;"
 button_css = "background-color: #24303f; border-width: 1px; border-radius: 10px; border-color: #24303f; font: 12px; margin:5px; color: #c2cdd9;"
